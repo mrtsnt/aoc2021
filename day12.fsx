6df022b98b3fcae52b156205e26e9a73d5cd3a8d
Martynas Seniut <martynas.seniut@gmail.com>
2021-12-12 13:16:53 +0200

    day12 part2 done

--- day12.fsx
+++ day12.fsx
@@ -1,58 +1,42 @@
 open System.IO
- 
-let data =
+
+let edges =
     File.ReadAllLines("data/day12")
     |> Array.map (fun r -> r.Split("-").[0], r.Split("-").[1])
-    |> List.ofArray
+    |> Array.collect (fun (s, e) ->
+        match s, e with
+        | "start", _ -> [|(s, e)|]
+        | _, "start" -> [|(e, s)|]
+        | "end", _ -> [|(e, s)|]
+        | _, "end" -> [|(s, e)|]
+        | _, _ -> [|(s, e); (e, s)|])
  
-let allEdges (data: (string * string) list) = 
-    let startEdges = 
-        List.filter (fun (a, b) -> a = "start" || b = "start") data
-        |> List.map (fun (a, b) -> if b = "start" then (b, a) else (a, b))
-    let endEdges = 
-        List.filter (fun (a, b) -> a = "end" || b = "end") data 
-        |> List.map (fun (a, b) -> if a = "end" then (b, a) else (a, b))
-    let other = 
-        List.filter (fun (a, b) -> a <> "start" && a <> "end" && b <> "start" && b <> "end") data
-        |> List.collect (fun (a, b) -> [(a, b); (b, a)])
-    startEdges @ endEdges @ other
- 
-let part1 (data: (string * string) list) =
-    let edges = allEdges data
- 
-    let rec findAllPaths (paths: (Set<string> * string list) list) : (Set<string> * string list) list =
-        List.collect(fun (visited, path) ->
-            let nextNodes = List.filter (fun (s, e) -> List.head path = s && not (Set.contains e visited)) edges
-            if List.isEmpty nextNodes then [(visited, path)]
-            else
-                List.collect (fun (_, (e: string)) ->
-                    let newVisited = if e.ToLower() = e then Set.add e visited else visited
-                    findAllPaths [(newVisited, e::path)]
-                ) nextNodes
-        ) paths
+let solve (edges: (string * string)[]) canVisit visit =
+    let rec traverse pos visited =
+        let nextNodes = Array.filter (fun (s, e) -> pos = s && canVisit e visited) edges
+        match Array.isEmpty nextNodes, pos with
+        | true, "end" -> 1
+        | true,  _ -> 0
+        | _ -> Array.sumBy (fun (_, e) -> traverse e (visit e visited)) nextNodes
 
-    findAllPaths [(Set.empty, ["start"])]
-    |> List.map snd
-    |> List.filter (fun l -> List.head l = "end")
-    |> List.length
+    traverse "start" Set.empty
 
-let part2 (data: (string * string) list) =
-    let edges = allEdges data
- 
-    let rec findAllPaths (paths: (Set<string> * string list) list) : (Set<string> * string list) list =
-        List.collect(fun (visited, path) ->
-            let nextNodes = List.filter (fun (s, e) -> List.head path = s && not (Set.contains e visited)) edges
-            if List.isEmpty nextNodes then [(visited, path)]
-            else
-                List.collect (fun (_, (e: string)) ->
-                    let newVisited = if e.ToLower() = e then Set.add e visited else visited
-                    findAllPaths [(newVisited, e::path)]
-                ) nextNodes
-        ) paths
+let canVisit1 e visited = not (Set.contains e visited)
+let visit1 (e: string) visited = if e.ToLower() = e then Set.add e visited else visited
 
-    findAllPaths [(Set.empty, ["start"])]
-    |> List.map snd
-    |> List.filter (fun l -> List.head l = "end")
-    |> List.length
+let canVisit2 e (visited: Set<string>) = 
+    if Set.isEmpty visited then true
+    else
+        match Set.contains e visited, Set.minElement visited with
+        | true, "_" -> false
+        | true, _ -> true
+        | _ -> true
 
-part1 data
+let visit2 (e: string) visited = 
+    match e.ToLower() = e, Set.contains e visited with
+    | true, true -> Set.add "_" visited
+    | true, false -> Set.add e visited
+    | _ -> visited
+
+solve edges canVisit1 visit1 |> printfn "%d"
+solve edges canVisit2 visit2 |> printfn "%d"
