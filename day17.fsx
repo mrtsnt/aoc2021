1fe12ce1b9de2de2f64278e4f930acd86b71dba6
Martynas Seniut <martynas.seniut@gmail.com>
2021-12-17 11:59:38 +0200

    day17 brute force done

---
+++ day17.fsx
@@ -0,0 +1,28 @@
+let xMin, xMax, yMin, yMax = 124, 174, -123, -86
+let inRange (x, y) = x <= xMax && y >= yMin
+let inTarget (x, y) = inRange (x, y) &&  x >= xMin && y <= yMax
+
+let points xv yv = 
+    let rec impl x y (xv, yv) =
+        seq {
+            yield (x, y)
+            yield! impl (x + xv) (y + yv) ((if xv - 1 < 0 then 0 else xv - 1), (yv - 1))
+        }
+    impl 0 0 (xv, yv)
+
+let generateRange x x' y y' = Seq.collect(fun x -> [y..y'] |> Seq.map(fun y -> (x, y))) [x..x']
+
+let part1 () =
+    generateRange 1 1000 1 1000
+    |> Seq.map (fun (xv, yv) ->
+        let allPoints = points xv yv |> Seq.takeWhile inRange |> Array.ofSeq
+        if inTarget <| Array.last allPoints then Array.maxBy snd allPoints |> snd else 0)
+    |> Seq.max
+
+let part2 () =
+    generateRange 1 1000 -1000 1000
+    |> Seq.filter (fun (xv, yv) -> points xv yv |> Seq.takeWhile inRange |> Seq.last |> inTarget)
+    |> Seq.length
+
+part1 () |> printfn "%d"
+part2 () |> printfn "%d"
